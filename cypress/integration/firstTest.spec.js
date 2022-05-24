e65e7efdf1868ead7b48551433e138845111bd87
Kiryl Chaikouski <sybershot.1998@gmail.com>
2022-05-24 15:19:53 +0300

    Implemented radio buttons and checkbox.

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -109,7 +109,7 @@ describe('Our first suite', () => {
             })
     })
 
-    it.only('Assert propety', () => {
+    it('Assert propety', () => {
         
         cy.contains('Datepicker').click()
 
@@ -118,6 +118,33 @@ describe('Our first suite', () => {
             cy.get('nb-calendar-day-picker').contains('17').click()
             cy.wrap(input).invoke('prop', 'value').should('contain', 'May 17, 2022')
         })
+    })
+
+    it.only('Radio buttons and checkbox', () => {
+
+        cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
+
+            cy.wrap(radioButtons)
+                .first()
+                .check({force: true})
+                .should('be.checked')
+
+            cy.wrap(radioButtons)
+                .eq(1)
+                .check({force: true})
+                .should('be.checked')
+            
+                cy.wrap(radioButtons)
+                .eq(2)
+                .should('be.disabled')
+        })
+
+        cy.contains('Modal & Overlays').click()
+        cy.contains('Toastr').click()
+
+        // cy.get('[type="checkbox"]').check({force: true})
+        cy.get('[type="checkbox"]').eq(0).click({force: true})
+        cy.get('[type="checkbox"]').eq(1).check({force: true})
     })
 })
 
