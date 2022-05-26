871b5be2a5b251cf56fccb3ecce3913dce84a30a
Kiryl Chaikouski <sybershot.1998@gmail.com>
2022-05-26 10:54:59 +0300

    Implemented Lists and dropdowns.

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -120,7 +120,7 @@ describe('Our first suite', () => {
         })
     })
 
-    it.only('Radio buttons and checkbox', () => {
+    it('Radio buttons and checkbox', () => {
 
         cy.contains('nb-card', 'Using the Grid').find('[type="radio"]').then( radioButtons => {
 
@@ -145,6 +145,39 @@ describe('Our first suite', () => {
         // cy.get('[type="checkbox"]').check({force: true})
         cy.get('[type="checkbox"]').eq(0).click({force: true})
         cy.get('[type="checkbox"]').eq(1).check({force: true})
+    })
+
+    it.only('Lists and dropdowns', () => {
+        cy.visit('/')
+
+        //1
+        // cy.get('nav nb-select').click()
+        // cy.get('.options-list').find('[ng-reflect-value="dark"]').click()
+        // cy.get('nav nb-select').should('contain', 'Dark')
+        // cy.get('nb-layout-header nav').should('have.css', 'background-color', 'rgb(34, 43, 69)')
+
+        //2
+        cy.get('nav nb-select').then( dropdown => {
+            cy.wrap(dropdown).click()
+            cy.get('.options-list nb-option').each( (listItem, index) => {
+                const itemText = listItem.text().trim()
+
+                const colors = {
+                    "Light": "rgb(255, 255, 255)",
+                    "Dark": "rgb(34, 43, 69)",
+                    "Cosmic": "rgb(50, 50, 89)",
+                    "Corporate": "rgb(255, 255, 255)"
+                }
+
+                cy.wrap(listItem).click()
+                cy.wrap(dropdown).should('contain', itemText)
+                cy.get('nb-layout-header nav').should('have.css', 'background-color', colors[itemText])
+                if( index < 3) {
+                    cy.wrap(dropdown).click()
+                }
+                
+            })
+        })
     })
 })
 
